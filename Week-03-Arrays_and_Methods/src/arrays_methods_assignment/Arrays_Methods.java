65d75aa518f95838a0655ac717a8bb8721886d59
Brandon <bdelaro12@gmail.com>
2022-09-24 23:40:45 -0400

    fourth commit

---
+++ Week-03-Arrays_and_Methods/src/arrays_methods_assignment/Arrays_Methods.java
@@ -0,0 +1,37 @@
+package arrays_methods_assignment;
+
+public class Arrays_Methods {
+
+	public static void main(String[] args) {
+		question1();
+		question2();
+		question3();
+		question4();
+		question5();
+		
+	}
+
+	public static void question1() {
+		
+	}
+	
+//	Change from void to String[] return type to use in question5 method
+	public static String[] question2(){
+		
+		
+		return null;
+	}
+
+	public static void question3(){
+		
+	}
+	
+	public static void question4(){
+		
+	}
+	
+	public static void question5(){
+		
+	}
+	
+}
