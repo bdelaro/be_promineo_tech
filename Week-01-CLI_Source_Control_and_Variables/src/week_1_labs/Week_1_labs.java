853f613b393b23b6fba96098f3de6a2039c20179
Brandon <bdelaro12@gmail.com>
2022-12-31 04:31:56 -0500

    labs update

--- Week-01-CLI_Source_Control_and_Variables/src/week_1_labs/Week_1_labs.java
+++ Week-01-CLI_Source_Control_and_Variables/src/week_1_labs/Week_1_labs.java
@@ -1,7 +1,5 @@
 package week_1_labs;
 
-import javax.print.attribute.standard.MediaSize.ISO;
-
 //Variables and Operations
 public class Week_1_labs {
 	public static void main(String[] args) {
@@ -9,17 +7,23 @@ public class Week_1_labs {
 		// create a variable to hold the quantity of available plane seats left on a
 		// flight
 		int availablePlaneSeats = 7;
+
 		// create a variable to hold the cost of groceries
 		double costOfGroceries = 12.99;
+
 		// create a variable to hold a person's middle initial
 		char middleInitial = 'A';
+
 		// create a variable to hold true if it's hot outside and false if it's cold
 		// outside
 		boolean isHotOutside = false;
+
 		// create a variable to hold a customer's first name
 		String customerFirstName = "Adam";
+
 		// create a variable to hold a street address
 		String streetAddress = "123 W 123rd St";
+
 		// Print all variables to the console
 		System.out.println("Available seats left on the plane: " + availablePlaneSeats);
 		System.out.println(costOfGroceries + " is the price for groceries.");
@@ -32,21 +36,26 @@ public class Week_1_labs {
 		// variable
 		availablePlaneSeats -= 2;
 		System.out.println(availablePlaneSeats);
+
 		// impulse candy bar purchase, add 2.15 to the grocery total
 		costOfGroceries += 2.15;
 		System.out.println(costOfGroceries);
+
 		// birth certificate was printed incorrectly, change the middle initial to
 		// something else
 		middleInitial = 'D';
 		System.out.println(middleInitial);
+
 		// the season has changed, update the hot outside variable to be opposite of
 		// what it was
 		isHotOutside = true;
 		System.out.println(isHotOutside);
+
 		// create a new variable called full name using the customer's first name, the
 		// middle initial, and a last name of your choice
 		String fullName = customerFirstName + " " + middleInitial + " Bates";
 		System.out.println(fullName);
+
 		// print a line to the console that introduces the customer and says they live
 		// at the address variable
 		System.out.println("This is " + fullName + ", and they live at " + streetAddress);
--- Week-03-Arrays_and_Methods/src/week_3_labs/Week_3_Labs.java
+++ Week-03-Arrays_and_Methods/src/week_3_labs/Week_3_Labs.java
@@ -1,5 +1,184 @@
 package week_3_labs;
 
 public class Week_3_Labs {
+	public static void main(String[] args) {
+		// create an array with the following values 1,5,2,8,13,6
+		int[] array = { 1, 5, 2, 8, 13, 6 };
 
+		// print out the first element in the array
+		System.out.println(array[0]);
+
+		// print out the last element in the array without using the number 5
+		System.out.println(array[array.length - 1]);
+
+		// print out the element in the array at position 6. What happens?
+		// System.out.println(array[array.length]); zero based, the last element will
+		// always be length of array minus 1, out of bounds
+
+		// print out the element in the array at position -1. What happens?
+		// System.out.println(array[-1]); arrays start at 0, out of bounds
+
+		// write a traditional for loop that prints out each element in the array
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+		// write an enhanced for loop that prints out each element in the array
+		for (int number : array) {
+			System.out.println(number);
+		}
+
+		// create a new variable called sum, write a loop that adds each element in the
+		// array to the sum
+		double sum = 0;
+		for (int number : array) {
+			sum += number;
+		}
+		System.out.println(sum);
+
+		// create a new variable called average and assign the average value of the
+		// array to it
+		double average = 0;
+		average = sum / array.length;
+		System.out.println(average);
+
+		// write an enhanced for loop that prints out each number in the array only if
+		// the number is odd
+		for (int number : array) {
+			if (number % 2 == 1) {
+				System.out.println(number);
+			}
+		}
+
+		// create an array that contains the values "Sam", "Sally", "Thomas", and
+		// "Robert"
+		String[] arrayNames = { "Sam", "Sally", "Thomas", "Robert" };
+
+		// calculate the sum of all the letters in the new array
+		int sumOfLetters = 0;
+		for (String x : arrayNames) {
+			sumOfLetters += x.length();
+		}
+		System.out.println(sumOfLetters);
+
+		// write and test a method that takes a String name and prints out a greeting,
+		// this method returns nothing
+		greet("Adam");
+		greet("Alex");
+
+		// write and test a method that takes a String name and returns a greeting, do
+		// not print in the method
+		greet2("Bob");
+		System.out.println(greet2("Bob"));
+
+		// analyze the difference between these two methods - what do you find? How are
+		// they different?
+		// The first one takes the input, performs the task on the input, and then
+		// prints it out, the second option just takes the input, performs the task on
+		// the input, then returns the result, leaving the last step open-ended for
+		// other actions to be
+		// performed on it, if needed
+
+		// write and test a method that takes a String and an int and returns true if
+		// the number of letters in the string is greater than the int
+		System.out.println(isStringLongerThanNumber("bob", 2));
+
+		// write and test a method that takes an array of string and a string and
+		// returns true if the string passed in exists in the array
+		String[] strings = { "blue", "chicken", "fries" };
+		System.out.println(isStringInArray(strings, "red"));
+
+		// write and test a method that takes an array of int and returns the smallest
+		// number in the array
+		System.out.println(smallestNumberInArray(array));
+
+		// write and test a method that takes an array of double and returns the average
+		double[] doubles = { 1, 2.3, 3 };
+		System.out.println(averageOfDoubleArray(doubles));
+
+		// write and test a method that takes an array of Strings and returns an array
+		// of int where each element
+		// matches the length of the string at that position
+		for (int x : lengthOfEachStringArrayElement(arrayNames)) {
+			System.out.println(x);
+		}
+
+		// write and test a method that takes an array of strings and returns true if
+		// the sum of letters for all strings with an
+		// even amount of letters is greater than the sum of those with an odd amount of
+		// letters.
+		System.out.println(evenOrOddGreater(arrayNames));
+
+		// write and test a method that takes a string and returns true if the string is
+		// a palindrome
+	}
+
+	public static void greet(String name) {
+		System.out.println("Hi, my name is " + name + ".");
+	}
+
+	public static String greet2(String name) {
+		return "Hi, my name is " + name + ".";
+	}
+
+	public static boolean isStringLongerThanNumber(String word, int number) {
+		return word.length() > number;
+	}
+
+	public static boolean isStringInArray(String[] strings, String string) {
+		for (String x : strings) {
+			if (x.equals(string)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static int smallestNumberInArray(int[] array) {
+		int smallest = array[0];
+		for (int x : array) {
+			if (smallest > x) {
+				smallest = x;
+			}
+		}
+		return smallest;
+	}
+
+	public static double averageOfDoubleArray(double[] array) {
+		double sum = 0;
+		for (double x : array) {
+			sum += x;
+		}
+		return sum / array.length;
+	}
+
+	public static int[] lengthOfEachStringArrayElement(String[] strings) {
+		int[] lengthOfElements = new int[strings.length];
+		for (int i = 0; i < strings.length; i++) {
+			lengthOfElements[i] = strings[i].length();
+		}
+		return lengthOfElements;
+	}
+
+	public static boolean evenOrOddGreater(String[] strings) {
+		int evenSums = 0, oddSums = 0;
+		for (String x : strings) {
+			if (x.length() % 2 == 0) {
+				evenSums += x.length();
+			} else {
+				oddSums += x.length();
+			}
+		}
+		return evenSums > oddSums;
+	}
+	
+	public static boolean checkForPalindrome(String string) {
+		for (int i = 0; i < string.length() / 2; i++) {
+			if (string.charAt(i) != string.charAt(string.length() - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+		
 }
